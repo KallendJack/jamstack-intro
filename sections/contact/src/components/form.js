1c2ce2ccba7df7371a1260eb3148616cfb26a792
Jack Kallend <Kallendo96@live.co.uk>
2020-02-28 15:51:36 +0000

    Added form

--- sections/contact/src/components/form.js
+++ sections/contact/src/components/form.js
@@ -13,66 +13,98 @@ const Form = () => {
 
   const onSubmit = e => {
     e.preventDefault()
-    updateStatus()
+    updateStatus('PENDING')
+
+    setTimeout(() => {
+      updateStatus('SUCCESS')
+    }, 1500)
   }
 
   const onChange = e => {
     setFormData({ ...formData, [e.target.name]: [e.target.value] })
   }
 
-  const updateStatus = () => {
-    setStatus('SUCCESS')
+  const updateStatus = status => {
+    setStatus(status)
+  }
+
+  const resetStatus = () => {
+    setFormData({
+      formData: {
+        name: '',
+        email: '',
+        subject: '',
+        body: ''
+      }
+    })
+    setStatus('IDLE')
   }
 
   if (status === 'SUCCESS') {
-    return <p className={styles.success}>Message sent!</p>
+    return (
+      <p className={styles.success}>
+        Message sent!
+        <button
+          className={`${styles.button} ${styles.centered}`}
+          onClick={resetStatus}
+        >
+          Reset
+        </button>
+      </p>
+    )
   }
 
   const { name, email, subject, body } = formData
 
   return (
-    <form className={styles.form} onSubmit={onSubmit}>
-      <label className={styles.label}>
-        Name
-        <input
-          type="text"
-          name="name"
-          className={styles.input}
-          value={name}
-          onChange={onChange}
-        />
-      </label>
-      <label className={styles.label}>
-        Email
-        <input
-          type="email"
-          name="email"
-          className={styles.input}
-          value={email}
-          onChange={onChange}
-        />
-      </label>
-      <label className={styles.label}>
-        Subject
-        <input
-          type="text"
-          name="subject"
-          className={styles.input}
-          value={subject}
-          onChange={onChange}
-        />
-      </label>
-      <label className={styles.label}>
-        Body
-        <textarea
-          name="body"
-          className={styles.input}
-          value={body}
-          onChange={onChange}
-        />
-      </label>
-      <button className={styles.button}>Send</button>
-    </form>
+    <>
+      {status === 'ERROR' && <p className={styles.error}>There were errors!</p>}
+      <form
+        className={`${styles.form} ${status === 'PENDING' && styles.pending}`}
+        onSubmit={onSubmit}
+      >
+        <label className={styles.label}>
+          Name
+          <input
+            type="text"
+            name="name"
+            className={styles.input}
+            value={name}
+            onChange={onChange}
+          />
+        </label>
+        <label className={styles.label}>
+          Email
+          <input
+            type="email"
+            name="email"
+            className={styles.input}
+            value={email}
+            onChange={onChange}
+          />
+        </label>
+        <label className={styles.label}>
+          Subject
+          <input
+            type="text"
+            name="subject"
+            className={styles.input}
+            value={subject}
+            onChange={onChange}
+          />
+        </label>
+        <label className={styles.label}>
+          Body
+          <textarea
+            name="body"
+            className={styles.input}
+            value={body}
+            onChange={onChange}
+          />
+        </label>
+        <button className={styles.button}>Send</button>
+      </form>
+    </>
   )
 }
 
